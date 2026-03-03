b392011dfadf1444d4a92cf751b93bb4b228cb0a
gabifreitassilva-pixel <gabifreitassilva@gmail.com>
2026-03-02 22:02:43 -0300

    Update service worker cache name and event listeners

--- sw.js
+++ sw.js
@@ -1,19 +1,10 @@
-const CACHE_NAME = 'shopee-v6-cache';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.3.2/papaparse.min.js'
-];
+const CACHE_NAME = 'shopee-pro-v7';
+const urls = ['./', './index.html', './manifest.json', 'https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.3.2/papaparse.min.js'];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urls)));
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
